c4f69daa039bb630c519b32fafb92681b9e45069
Sahil26299 <sahillokhande94@gmail.com>
2026-01-06 23:42:35 +0530

    typing indication added

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,12 @@ const REDIS_URL = process.env.REDIS_URL;
 
 const redis = new Redis(REDIS_URL || "");
 
-export const socketEvents = {
+const socketEvents = {
   JOIN_USER_ROOM: "join-user-room", // user logs in to dashboard (to listen new chats)
   JOIN_CHAT_ROOM: "join-chat-room", // user opens a chat (to listen messages in that chat)
   SEND_MESSAGE: "send-message",
+  START_TYPING: "start-typing",
+  USER_START_TYPING: "user-start-typing",
   RECEIVE_MESSAGES_IN_CHAT: "chat-message", // when user is viewing messages inside a particular chat
   RECEIVE_NEW_CHAT: "user-message", // when user is on dashboard, without any of the chat opened and new message is received
   USER_ONLINE: "user-online", // when user logs in to dashboard
@@ -18,9 +20,15 @@ export const socketEvents = {
   DISCONNECT: "disconnect",
 };
 
+const roomKeys = {
+  USER_ROOM: (val: string) => `user-room:${val}`,
+  CHAT_ROOM: (val: string) => `chat-room:${val}`,
+};
+
 export const redisKeys = {
   ONLINE_USERS: "online-users",
   USER_SOCKET_MAP: "user-socket-map",
+  USER_TYPING: (chatId: string, userId: string) => `typing-${chatId}:${userId}`,
 };
 
 const httpServer = createServer();
@@ -36,13 +44,12 @@ io.on("connection", (socket) => {
 
   // join user room (dashboard)
   socket.on(socketEvents.JOIN_USER_ROOM, async (userId: string) => {
-    // console.log("user joined the user room:", userId, "socket:", socket.id);
-
+    // get all socket-user mapping
     const existingUserSocketMap = await redis.hgetall(
       redisKeys.USER_SOCKET_MAP
     );
-    // console.log("userSocketMap:", Object.values(existingUserSocketMap));
 
+    // if any mapping with userId already exists (once or more), remove all such mappings
     if (Object.values(existingUserSocketMap).includes(userId)) {
       // Remove user from online users set
       await redis.srem(redisKeys.ONLINE_USERS, userId);
@@ -51,7 +58,7 @@ io.on("connection", (socket) => {
       const prevSocketIds = Object.entries(existingUserSocketMap)
         ?.filter((entr) => entr[1] === userId)
         ?.map((entr) => entr[0]);
-      // console.log("Deleting socket id: ", prevSocketIds);
+      // remove all mappings of that user
       if (prevSocketIds?.length > 0) {
         for (let index = 0; index < prevSocketIds.length; index++) {
           const prevSocketId = prevSocketIds[index];
@@ -62,45 +69,59 @@ io.on("connection", (socket) => {
       // Emit user offline to all users
       socket.broadcast.emit(socketEvents.USER_OFFLINE, userId);
     }
+    // now create a new mapping with user: userId and store it in online set
     // Add user to online users set
     await redis.sadd(redisKeys.ONLINE_USERS, userId);
-    // console.log("user added to online users set");
 
     // Map online user to socket id, to in turn receive that user based on socket id
     await redis.hset(redisKeys.USER_SOCKET_MAP, socket.id, userId);
-    // console.log("user added to user-socket-map");
 
     // Emit user online to all users
     socket.broadcast.emit(socketEvents.USER_ONLINE, userId);
 
     // Join user room
-    socket.join(`user-room:${userId}`);
+    socket.join(roomKeys.USER_ROOM(userId));
   });
 
   // join chat room (chat)
   socket.on(socketEvents.JOIN_CHAT_ROOM, (chatId: string) => {
     // console.log("user joined the chat:", chatId, "socket:", socket.id);
-    socket.join(`chat-room:${chatId}`);
+    socket.join(roomKeys.CHAT_ROOM(chatId));
   });
+
+  // start typing event
+  socket.on(
+    socketEvents.START_TYPING,
+    async (chatId: string, userId: string) => {
+      // console.log("user typing:", roomKeys.CHAT_ROOM(chatId));
+      
+      await redis.set(redisKeys.USER_TYPING(chatId, userId), 1, "EX", 3);
+      socket
+        .to(roomKeys.CHAT_ROOM(chatId))
+        .emit(socketEvents.USER_START_TYPING, { userId });
+    }
+  );
 
   // send a message
   socket.on(
     socketEvents.SEND_MESSAGE,
     (msg: any, toUserId: string, chatId: string) => {
       // emit message to user
-      socket.to(`user-room:${toUserId}`).emit(socketEvents.RECEIVE_NEW_CHAT, {
-        // lastMessage: msg?.text,
-        lastMessage: {
-          sentAt: msg?.createdAt,
-          text: msg?.text,
-          sender: msg?.sender,
-        },
-        chatId,
-        unreadIncrement: 1,
-      });
+      socket
+        .to(roomKeys.USER_ROOM(toUserId))
+        .emit(socketEvents.RECEIVE_NEW_CHAT, {
+          // lastMessage: msg?.text,
+          lastMessage: {
+            sentAt: msg?.createdAt,
+            text: msg?.text,
+            sender: msg?.sender,
+          },
+          chatId,
+          unreadIncrement: 1,
+        });
       // emit message to all users in the chat
       socket
-        .to(`chat-room:${chatId}`)
+        .to(roomKeys.CHAT_ROOM(chatId))
         .emit(socketEvents.RECEIVE_MESSAGES_IN_CHAT, msg);
     }
   );
